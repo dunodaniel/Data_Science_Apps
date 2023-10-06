1f9e2ae496bf97594ed2bb13a5171d67bb528434
Daniel V.O. Rodrigues <daniel.rodrigues46@fatec.sp.gov.br>
2023-10-06 03:58:36 +0000

    06-10-2023

--- apps/projeto2.py
+++ apps/projeto2.py
@@ -1,69 +1,38 @@
 import streamlit as st
 import pandas as pd
-import base64
-import matplotlib.pyplot as plt
-import seaborn as sns
 import numpy as np
 
 def exibir_projeto_2():
-    st.title('NBA Player Stats Explorer')
 
-    st.markdown("""
-    This app performs simple webscraping of NBA player stats data!
-    * **Python libraries:** base64, pandas, streamlit
-    * **Data source:** [Basketball-reference.com](https://www.basketball-reference.com/).
-    """)
+    st.title('Uber pickups in NYC')
 
-    st.sidebar.header('User Input Features')
-    selected_year = st.sidebar.selectbox('Year', list(reversed(range(1950,2020))))
+    DATE_COLUMN = 'date/time'
+    DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+                'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
-    # Web scraping of NBA player stats
     @st.cache_data
-    def load_data(year):
-        url = "https://www.basketball-reference.com/leagues/NBA_" + str(year) + "_per_game.html"
-        html = pd.read_html(url, header = 0)
-        df = html[0]
-        raw = df.drop(df[df.Age == 'Age'].index) # Deletes repeating headers in content
-        raw = raw.fillna(0)
-        playerstats = raw.drop(['Rk'], axis=1)
-        return playerstats
-    playerstats = load_data(selected_year)
+    def load_data(nrows):
+        data = pd.read_csv(DATA_URL, nrows=nrows)
+        lowercase = lambda x: str(x).lower()
+        data.rename(lowercase, axis='columns', inplace=True)
+        data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+        return data
 
-    # Sidebar - Team selection
-    sorted_unique_team = sorted(playerstats.Tm.unique())
-    selected_team = st.sidebar.multiselect('Team', sorted_unique_team, sorted_unique_team)
+    data_load_state = st.text('Loading data...')
+    data = load_data(10000)
+    data_load_state.text("Done! (using st.cache_data)")
 
-    # Sidebar - Position selection
-    unique_pos = ['C','PF','SF','PG','SG']
-    selected_pos = st.sidebar.multiselect('Position', unique_pos, unique_pos)
+    if st.checkbox('Show raw data'):
+        st.subheader('Raw data')
+        st.write(data)
 
-    # Filtering data
-    df_selected_team = playerstats[(playerstats.Tm.isin(selected_team)) & (playerstats.Pos.isin(selected_pos))]
+    st.subheader('Number of pickups by hour')
+    hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+    st.bar_chart(hist_values)
 
-    st.header('Display Player Stats of Selected Team(s)')
-    st.write('Data Dimension: ' + str(df_selected_team.shape[0]) + ' rows and ' + str(df_selected_team.shape[1]) + ' columns.')
-    st.dataframe(df_selected_team)
+    # Some number in the range 0-23
+    hour_to_filter = st.slider('hour', 0, 23, 17)
+    filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
-    # Download NBA player stats data
-    # https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
-    def filedownload(df):
-        csv = df.to_csv(index=False)
-        b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
-        href = f'<a href="data:file/csv;base64,{b64}" download="playerstats.csv">Download CSV File</a>'
-        return href
-
-    st.markdown(filedownload(df_selected_team), unsafe_allow_html=True)
-
-    # Heatmap
-    if st.button('Intercorrelation Heatmap'):
-        st.header('Intercorrelation Matrix Heatmap')
-        df_selected_team.to_csv('output.csv',index=False)
-        df = pd.read_csv('output.csv')
-
-        corr = df.corr()
-        mask = np.zeros_like(corr)
-        mask[np.triu_indices_from(mask)] = True
-        with sns.axes_style("white"):
-            f, ax = plt.subplots(figsize=(7, 5))
-            ax = sns.heatmap(corr, mask=mask, vmax=1, square=True)
-        st.pyplot()
+    st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+    st.map(filtered_data)
--- main.py
+++ main.py
@@ -4,6 +4,13 @@ from apps.projeto2 import exibir_projeto_2
 
 # Configuração do cabeçalho
 st.title("Meus Projetos Streamlit")
+st.header("Bem-vindo à minha página de projetos!")
+
+# Breve apresentação
+st.write("Olá! Sou [DunoDaniel](https://github.com/) e estou empolgado em compartilhar meus projetos com você.")
+st.write("Nesta página, você encontrará dois dos meus projetos mais recentes.")
+st.write("Sinta-se à vontade para explorar e experimentar cada projeto clicando nos botões à esquerda.")
+
 
 # Cria links para os projetos
 projeto1 = st.sidebar.button("Projeto 1")
